26418abec46c25b1aeb097689c64090329bf3461
choitoday <healutoday@naver.com>
2023-12-30 15:40:24 +0900

    완료

---
+++ 5~6회차/js복습과제/계산기/계산기.js
@@ -0,0 +1,27 @@
+var numberClicked = false;
+function add (char) { 
+    if(numberClicked == false){
+    if(isNaN(char) == true){
+    } else { 
+        document.getElementById('display').value += char;
+        }
+    } else {
+    document.getElementById('display').value += char;
+    }
+    if(isNaN(char) == true) {
+         numberClicked = false; 
+     } else { 
+        numberClicked = true; 
+    }
+ }
+
+function calculate() {var display = document.getElementById('display');
+ var result = eval(display.value);
+document.getElementById('result').value = result;
+}
+
+function reset(){
+    document.getElementById('display').value = "";
+    document.getElementById('result').value = "";
+}
+
